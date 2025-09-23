8699a8d039e6fe8bfe1d76bb6ee73142f58cef80
putra <syahputratirta.001@gmail.com>
2025-09-23 13:52:44 +0700

    halaman about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'About')
 
 @section('content')
-    <section>
+    <section class="p-14">
         {{-- baris 1 --}}
-        <div class="flex justify-center gap-4 p-14">
+        <div class="flex justify-center gap-4 mb-4">
             <a href="" class="w-1/3 h-[450px]">
                 <div class="w-full h-full flex flex-col">
                     <img src="{{ asset('images/about-1.webp') }}" alt="About 1" class="w-full h-2/3 object-cover">
@@ -42,6 +42,174 @@
                         </h1>
                     </div>
                 </div>
+            </a>
+        </div>
+        {{-- baris 2 --}}
+        <div class="flex justify-center gap-4 mb-4">
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-4.webp') }}" alt="About 1" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">8/27/24</h3>
+                        <h1 class="font-bold text-lg">
+                            Genap Berusia 7 Tahun, Kopi Kenangan Umumkan Pencapaian dan Rencana Ekspansi Internasional
+                            Selanjutnya
+                        </h1>
+                    </div>
+                </div>
+            </a>
+
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-5.webp') }}" alt="About 2" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">6/26/24</h3>
+                        <h1 class="font-bold text-lg">
+                            Kolaborasi dengan Komikus Lokal, Kopi Kenangan Hadirkan Bajak Laut Bertopi Jerami di Gerainya
+                        </h1>
+                    </div>
+                </div>
+            </a>
+
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-6.webp') }}" alt="About 3" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">6/5/24</h3>
+                        <h1 class="font-bold text-lg">
+                            Bonjour! Memperkenalkan Kenangan Boulangerie, Yang Terbaru dari Kenangan Brands
+                        </h1>
+                    </div>
+                </div>
+            </a>
+        </div>
+        {{-- baris 3 --}}
+        <div class="flex justify-center gap-4 mb-4">
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-7.webp') }}" alt="About 1" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">5/6/24</h3>
+                        <h1 class="font-bold text-lg">
+                            Hari Pendidikan Nasional 2024, Kopi Kenangan Bersama Yayasan 1000 Guru Mengunjungi Garut, Jawa
+                            Barat
+                        </h1>
+                    </div>
+                </div>
+            </a>
+
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-8.webp') }}" alt="About 2" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">5/2/24</h3>
+                        <h1 class="font-bold text-lg">
+                            “Satu Nusa, Satu Bangsa, Satu Kenangan” Kenangan Brands Hadirkan Kopi Bergaya Italian Roast yang
+                            Terjangkau dengan Peluang Kemitraan </h1>
+                    </div>
+                </div>
+            </a>
+
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-9.webp') }}" alt="About 3" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">3/19/24</h3>
+                        <h1 class="font-bold text-lg">
+                            Kopi Kenangan Hadirkan Menu Blewah Mewah
+                        </h1>
+                    </div>
+                </div>
+            </a>
+        </div>
+        {{-- baris 4 --}}
+        <div class="flex justify-center gap-4 mb-4">
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-10.webp') }}" alt="About 1" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">12/14/23</h3>
+                        <h1 class="font-bold text-lg">
+                            Kenangan Brands Paparkan Capaian CSR Sepanjang Tahun 2023 melalui Kenangan BAIK
+                        </h1>
+                    </div>
+                </div>
+            </a>
+
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-11.webp') }}" alt="About 2" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">
+                            9/26/23</h3>
+                        <h1 class="font-bold text-lg">
+                            Kenangan Coffee, New Retail F&B Unicorn Pertama di Asia Tenggara, Mengambil Langkah Strategis
+                            Memasuki Pasar Singapura
+                        </h1>
+                    </div>
+                </div>
+            </a>
+
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-12.webp') }}" alt="About 3" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">
+                            8/31/23
+                        </h3>
+                        <h1 class="font-bold text-lg">
+                            Kopi Kenangan Hanya Untukmu Hadir Dalam Perjalanan Petualangan Sherina 2
+                        </h1>
+                    </div>
+                </div>
+            </a>
+        </div>
+        {{-- baris 5 --}}
+        <div class="flex justify-center gap-4 mb-4">
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-13.webp') }}" alt="About 1" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">8/11/23</h3>
+                        <h1 class="font-bold text-lg">
+                            Rayakan HARMANAS Ke-6, CEO Kopi Kenangan dan Basboi Sajikan Kopi Kenangan Mantan untuk Pelanggan
+                            Setia karena U Da Best
+                        </h1>
+                    </div>
+                </div>
+            </a>
+
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-14.webp') }}" alt="About 2" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">
+                            5/17/23</h3>
+                        <h1 class="font-bold text-lg">
+                            Babak Baru Kenangan Brands, Fokus Menjadi Perusahaan F&B Berkelanjutan di Tahun 2030
+                        </h1>
+                    </div>
+                </div>
+            </a>
+
+            <a href="" class="w-1/3 h-[450px]">
+                <div class="w-full h-full flex flex-col">
+                    <img src="{{ asset('images/about-15.webp') }}" alt="About 3" class="w-full h-2/3 object-cover">
+                    <div class="p-2">
+                        <h3 class="text-sm text-gray-600">
+                            4/10/23
+                        </h3>
+                        <h1 class="font-bold text-lg">
+                            Makan Tanpa Ragu, Seluruh Produk Kenangan Brands telah Kantongi Sertifikasi Halal
+                        </h1>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="flex justify-end">
+            <a href="">
+                Older Posh >
             </a>
         </div>
     </section>
